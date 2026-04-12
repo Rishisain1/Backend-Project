73222868656b84920681b9b26e48e2778d7d1817
Rishi Saini <rishisaini604@gmail.com>
2026-04-12 14:32:41 +0530

    all set up and handle error

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser";
-
+import userRouter from "./routes/user.routes.js"
 
 const app= express();
 
@@ -14,5 +14,7 @@ app.use(express.json({limit:"16k"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+app.use("/api/v1/users", userRouter);
 
 export { app }
